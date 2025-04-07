3637588602fd77682ff860717deb4cce98513123
hrhrkeee <hrhrkeee@users.noreply.github.com>
2025-04-07 22:42:22 +0000

    deploy: d70c8dbbfc4c20ff4788ee53ff0197620177a097

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3141183929"
+    serviceWorkerVersion: "3964383621"
   }
 });
